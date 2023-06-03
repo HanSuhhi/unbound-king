0e57f4fe3f59d1d560efc11205c86c8acfca50d2
Don <l_98b@outlook.com>
2023-06-03 10:06:00 +0800

    feat(client-background): add background noise

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { NConfigProvider, NLoadingBarProvider, NMessageProvider, NNotificationProvider, dateZhCN, zhCN } from "naive-ui";
 import { storeToRefs } from "pinia";
-import { onMounted } from "vue";
 import { defineAppLayout } from "./components/app/composables/appLayout";
 import GlobalDialog from "./components/dialog/global/GlobalDialog.vue";
 import { bindNaiveUILayer, defineNaiveTheme } from "./composables/theme/naiveTheme";
@@ -12,9 +11,9 @@ import RouterHistory from "./components/routerHistory/RouterHistory.vue";
 import appAside from "./components/app/appAside/AppAside";
 import AppFooter from "./components/app/AppFooter.vue";
 import { disableDefaultKeys } from "./composables/experience/hotkey";
-import { useAuthStore } from "./stores/auth.store";
 import { dialogMessage } from "@/composables/components/globalDialog";
 import { useGlobalStore } from "@/stores/global.store";
+import Noise from "@/components/effects/BackgroundNoise.vue";
 
 const { Layout, renderLayout } = defineAppLayout();
 const { pageTransition } = storeToRefs(useGlobalStore());
@@ -24,14 +23,10 @@ provideStaticStyleVariables();
 useCorsor();
 bindNaiveUILayer();
 disableDefaultKeys();
-
-const { isSighIn } = storeToRefs(useAuthStore());
-onMounted(() => {
-  console.log("isSighIn: ", isSighIn.value);
-});
 </script>
 
 <template>
+  <noise />
   <n-config-provider class="h" preflight-style-disabled :locale="zhCN" :date-locale="dateZhCN" :theme="darkTheme" :theme-overrides="darkThemeOverrides">
     <n-loading-bar-provider>
       <n-message-provider>
