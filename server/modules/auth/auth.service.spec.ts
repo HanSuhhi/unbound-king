178b308e3c9c52af011dba441fc9608409f7d0c6
Don <l_98b@outlook.com>
2023-06-17 00:35:00 +0800

    test(server-auth): added util testings for auth-service:register

--- server/modules/auth/auth.service.spec.ts
+++ server/modules/auth/auth.service.spec.ts
@@ -3,13 +3,24 @@ import { Test } from "@nestjs/testing";
 import { CACHE_MANAGER } from "@nestjs/cache-manager";
 import type { Cache } from "cache-manager";
 import { getModelToken } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import type { Model } from "mongoose";
+import { it } from "vitest";
 import { User } from "../users/schemas/user.schemas";
 import { UsersService } from "../users/users.service";
 import { AuthService } from "./auth.service";
+import type { LoginDto } from "./dtos/login.dto";
+import { LoginRegistration } from "#/composables/constant/request";
 
 describe("AuthService", () => {
   const TEST_EMAIL = "test@example.com";
+  const TEST_CODE = 123456;
+  const TEST_USER: LoginDto = {
+    email: TEST_EMAIL,
+    code: TEST_CODE,
+    loginType: LoginRegistration.REGISTRATION
+  };
+  const email = "hello@test.com";
+
   let userModel: Model<User>;
   let service: AuthService;
   let cacheConfig: Cache;
@@ -23,13 +34,23 @@ describe("AuthService", () => {
         {
           provide: CACHE_MANAGER,
           useValue: {
-            get: () => "",
-            set: vi.fn()
+            get: (input) => {
+              switch (input) {
+                case TEST_EMAIL:
+                default:
+                  return TEST_CODE;
+              }
+            },
+            set: vi.fn(),
+            del: vi.fn()
           }
         },
         {
           provide: getModelToken(User.name),
-          useValue: Model
+          useValue: {
+            findOne: vi.fn(),
+            create: vi.fn()
+          }
         }
       ]
     }).compile();
@@ -66,7 +87,64 @@ describe("AuthService", () => {
     });
   });
 
-  // describe("register", () => {
+  describe("register", () => {
+    beforeEach(() => {
+      vi.spyOn(userModel, "findOne").mockReturnValue({
+        exec: vi.fn().mockResolvedValueOnce(TEST_USER)
+      } as any);
+    });
 
-  // });
+    it("should throw error if code does not match", async () => {
+      expect(service.register({
+        email: TEST_EMAIL,
+        code: 123455,
+        loginType: LoginRegistration.REGISTRATION
+      })).rejects.toThrowError();
+    });
+
+    it("should delete the code from the cache", async () => {
+      const del = vi.spyOn(cacheConfig, "del");
+
+      await service.register(TEST_USER).catch(() => {});
+
+      expect(del).toHaveBeenCalledTimes(1);
+      expect(del.mock.calls[0][0]).toEqual(TEST_EMAIL);
+    });
+
+    it("should throw error if user already exists", async () => {
+      const create = vi.spyOn(userModel, "create").mockResolvedValueOnce(email as any);
+
+      expect(service.register(TEST_USER)).rejects.toThrowError();
+
+      await service.register({
+        email,
+        code: TEST_CODE,
+        loginType: LoginRegistration.REGISTRATION
+      });
+      expect(create).toHaveBeenCalledTimes(1);
+      expect(create.mock.calls[0][0]).toEqual({
+        email
+      });
+      expect(create.mock.calls[0][0]).not.toEqual({
+        email: TEST_EMAIL
+      });
+    });
+
+    it("should create a new user", async () => {
+      vi.spyOn(userModel, "findOne").mockReturnValue({
+        exec: vi.fn().mockResolvedValueOnce(null)
+      } as any);
+      const create = vi.spyOn(userModel, "create").mockResolvedValueOnce(TEST_EMAIL as any);
+
+      await service.register({
+        email,
+        code: TEST_CODE,
+        loginType: LoginRegistration.REGISTRATION
+      });
+      expect(create).toHaveBeenCalledTimes(1);
+      expect(create.mock.calls[0][0]).toEqual({
+        email
+      });
+    });
+  });
 });
